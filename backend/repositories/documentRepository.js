ba6cd2c414600887a30d41f86b38b3343b99ff14
Ashish Sharma <ashishsharma90807@gmail.com>
2026-05-28 17:02:00 +0530

    feat: wrap multi-document operations in ACID transactions with session propagation

--- backend/repositories/documentRepository.js
+++ backend/repositories/documentRepository.js
@@ -1,25 +1,25 @@
 const Document = require('../models/Document');
 
 class DocumentRepository {
-  async create(documentData) {
+  async create(documentData, options = {}) {
     const doc = new Document(documentData);
-    return await doc.save();
+    return await doc.save(options);
   }
 
-  async findById(id) {
-    return await Document.findById(id);
+  async findById(id, options = {}) {
+    return await Document.findById(id, null, options);
   }
 
-  async update(id, updateData) {
-    return await Document.findByIdAndUpdate(id, updateData, { new: true });
+  async update(id, updateData, options = {}) {
+    return await Document.findByIdAndUpdate(id, updateData, { new: true, ...options });
   }
 
-  async findByUser(userId) {
-    return await Document.find({ user: userId }).sort({ createdAt: -1 });
+  async findByUser(userId, options = {}) {
+    return await Document.find({ user: userId }, null, options).sort({ createdAt: -1 });
   }
 
-  async delete(id) {
-    return await Document.findByIdAndDelete(id);
+  async delete(id, options = {}) {
+    return await Document.findByIdAndDelete(id, options);
   }
 }
 
